89de009f8dffb54753dd61ffc0ccf694ec45ef51
dingzhanghui <dingzhanghui1993@163.com>
2019-07-03 11:21:04 +0800

    first commit

---
+++ src/main/java/com/pretty/springcloudlearn/SpringcloudlearnApplication.java
@@ -0,0 +1,12 @@
+package com.pretty.springcloudlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudlearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudlearnApplication.class, args);
+    }
+}
