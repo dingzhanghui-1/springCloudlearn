503b78a1a83188368fc4b54b5d79c1c9719a5bfc
dingzhanghui <dingzhanghui1993@163.com>
2019-08-14 18:27:53 +0800

    增加cglib 相关的动态代理

---
+++ src/main/java/com/pretty/cglibproxy/CglibProxy.java
@@ -0,0 +1,46 @@
+package com.pretty.cglibproxy;
+
+import com.pretty.dynamicproxy.ISay;
+import com.pretty.dynamicproxy.Say;
+import lombok.extern.slf4j.Slf4j;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * 1.需要在项目中引入 cglib 版本jar 包
+ * 2.若为非maven 项目 还需引入asm jar包
+ * 若类为final 修饰的,则报错
+ * 若方法为final  修饰,则代理无效
+ * 代理方式(继承)
+ *
+ */
+@Slf4j
+public class CglibProxy  implements MethodInterceptor {
+
+    private Object target;
+
+    public CglibProxy(Object target) {
+        this.target = target;
+    }
+
+
+    public Object getProxy()
+    {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(target.getClass().getSuperclass());
+        enhancer.setCallback(this);
+        return enhancer.create();
+    }
+
+    @Override public Object intercept(Object target, Method method, Object[] objects, MethodProxy methodProxy)
+            throws Throwable {
+        log.info("cglibProxy start");
+        Object object = methodProxy.invokeSuper(target, objects);
+        log.info("cgkibProxy  end");
+        return object;
+    }
+
+}
--- src/main/java/com/pretty/dynamicproxy/JdkProxyTest.java
+++ src/main/java/com/pretty/dynamicproxy/JdkProxyTest.java
@@ -1,12 +1,21 @@
 package com.pretty.dynamicproxy;
 
+import com.pretty.cglibproxy.CglibProxy;
+
 public class JdkProxyTest {
 
-    public static  void main(String[] args)
+   /* public static  void main(String[] args)
     {
         Say say = new ISay();
         MyInvocationHandler handler = new MyInvocationHandler(say);
         Say sayproxy = (Say)handler.getProxy();
         sayproxy.say();
+    }*/
+
+    public static  void main(String[] args)
+    {
+        ISay Isay = new ISay();
+        CglibProxy cglibProxy = new CglibProxy(Isay);
+        cglibProxy.getProxy()
     }
 }
